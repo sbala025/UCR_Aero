d46e7cfd7433a8dabc07be41c52de0b9c292ee7b
sbala025 <shreyaaexc@gmail.com>
2021-03-31 16:42:01 -0700

    initial

---
+++ index.js
@@ -0,0 +1,37 @@
+$(function(){
+    $("#aboutUsCell_1").click(function() {
+       $("#aboutUsCell_1_Desc").toggle("slow");
+    });
+
+    $("#aboutUsCell_2").click(function() {
+        $("#aboutUsCell_2_Desc").toggle("slow");
+     });
+
+     $("#aboutUsCell_3").click(function() {
+        $("#aboutUsCell_3_Desc").toggle("slow");
+     });
+
+   //   $("#aboutUsCell_3").click(function() {
+   //      $("#aboutUsCell_3_Desc").toggle("slow");
+   //   });
+
+     $("#aboutUsCell_4").click(function() {
+        $("#aboutUsCell_4_Desc").toggle("slow");
+     });
+
+     $("#aboutUsCell_5").click(function() {
+        $("#aboutUsCell_5_Desc").toggle("slow");
+     });
+
+     $("#aboutUsCell_6").click(function() {
+        $("#aboutUsCell_6_Desc").toggle("slow");
+     });
+
+     $("#aboutUsCell_7").click(function() {
+        $("#aboutUsCell_7_Desc").toggle("slow");
+     });
+
+     $("#aboutUsCell_8").click(function() {
+        $("#aboutUsCell_8_Desc").toggle("slow");
+     });
+})
